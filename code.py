648e0404ca04252b7e2f21ea1d6c856e9ca31062
Sophie Nguyen <113055711+sophie-nguyen-0@users.noreply.github.com>
2025-04-11 09:55:27 -0400

    python code

---
+++ code.py
@@ -0,0 +1,36 @@
+# SPDX-FileCopyrightText: 2018 Kattni Rembor for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
+
+# Sophie
+#Apr 11
+#
+# Move a servo with a potentiometer in sync
+
+import time
+import board
+import pwmio
+import servo
+from analogio import AnalogIn
+
+# create a PWMOut object on Pin 16.
+# pwm: Pulse-width module
+# pwm is a measure of elapsed time between the leading and trailing edges of a single pulse of energy
+pwm = pwmio.PWMOut(board.GP16, duty_cycle=2 ** 15, frequency=50)
+
+# sets pot pin to pin 26/A0
+potentionmeter = AnalogIn(board.GP26) 
+
+# Create a servo object, my_servo.
+my_servo = servo.Servo(pwm, min_pulse = 650, max_pulse = 2500)
+
+while True:
+    potentiometer_value = potentionmeter.value
+    print(potentiometer_value)
+
+    # changing range to 1-180
+    angle = potentiometer_value * (180/65535)
+
+    #changes value of servo angle
+    my_servo.angle = angle
+    time.sleep(1)
